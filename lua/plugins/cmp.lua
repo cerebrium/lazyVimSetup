0ee166650ccee29d9b363e1a353cb3d5e759ca0f
Nicholas shankland <nicholasshankland@Nicholass-MacBook-Pro-2.local>
2024-04-11 10:07:30 -0700

    added inlay hints

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -78,33 +78,3 @@ return {
     })
   end,
 }
-
--- -- override nvim-cmp and add cmp-emoji
--- return {
---   "hrsh7th/nvim-cmp",
---   dependencies = { "hrsh7th/cmp-emoji" },
---   ---@param opts cmp.ConfigSchema
---   opts = function(_, opts)
---     local cmp = require("cmp")
---     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
---
---     cmp.setup({
---
---
---
---
---
---       mapping = cmp.mapping.preset.insert({
---         ["<C-n>"] = cmp.mapping.select_next_item(),
---         ["<C-p>"] = cmp.mapping.select_prev_item(),
---         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
---         ["<C-f>"] = cmp.mapping.scroll_docs(4),
---         ["<C-Space>"] = cmp.mapping.complete({}),
---         ["<CR>"] = cmp.mapping.confirm({
---           behavior = cmp.ConfirmBehavior.Replace,
---           select = true,
---         }),
---       }),
---     })
---   end,
--- }
---
+++ lua/plugins/rustextras.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    lazy = false,
+    config = function(_, opts)
+      require("nvim-dap-virtual-text").setup()
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = true },
+    },
+  },
+}
